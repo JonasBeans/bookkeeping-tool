4864d2c46185357222ecc4029e296d645130e34b
JonasBeans <boon.jonas.git@outlook.com>
2025-08-29 00:31:50 +0200

    Configure resource path resolver
    Signed-off-by: JonasBeans <boon.jonas.git@outlook.com>

--- src/main/java/be/jonasboon/book_keeping_tool/configuration/WebConfig.java
+++ src/main/java/be/jonasboon/book_keeping_tool/configuration/WebConfig.java
@@ -1,9 +1,14 @@
 package be.jonasboon.book_keeping_tool.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebMvc
@@ -11,7 +16,23 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/browser/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/browser/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location)
+                            throws IOException {
+
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        if (requestedResource.exists() && requestedResource.isReadable()) {
+                            return requestedResource;
+                        }
+
+                        return new ClassPathResource("/static/browser/index.html");
+
+                    }
+                });
     }
 }
 
