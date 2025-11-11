9ce555840d470eda3b5173a3e3933d5bd80e1b7b
JonasBeans <boon.jonas.git@outlook.com>
2025-11-11 12:34:29 +0100

    Filter out skip assigned cost centers in charts - REVISIT
    Revisited because it was not working correctly. Filter should have been set on income pie chart data.
    
    Signed-off-by: JonasBeans <boon.jonas.git@outlook.com>

--- frontend/bookkeeping/src/app/services/chart.service.ts
+++ frontend/bookkeeping/src/app/services/chart.service.ts
@@ -15,13 +15,11 @@ export class ChartService {
 	public load(costs: CostCenter[], incomes: CostCenter[]): void {
 		this.costs_pie_chart_data = costs
 			.filter(item => item.totalAmount > 0)
-			.filter(item => item.costCenter.toLowerCase() != 'skip')
 			.map<ChartPart>(item => {return {name: item.costCenter, value: item.totalAmount}});
 		this.income_pie_chart_data = incomes
 			.filter(item => item.totalAmount > 0)
+			.filter(item => item.costCenter.toLowerCase() != 'skip')
 			.map<ChartPart>(item => {return {name: item.costCenter, value: item.totalAmount}});
 	}
-
-
 
 }
