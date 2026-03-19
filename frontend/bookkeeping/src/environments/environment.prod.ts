683812675b4e1018ab8aad3e42b97d2b633c2b23
JonasBeans <boon.jonas.git@outlook.com>
2026-03-20 00:19:09 +0100

    Add port to base URL

--- frontend/bookkeeping/src/environments/environment.prod.ts
+++ frontend/bookkeeping/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@
 // Override at build time by editing this file or using a CI replacement step.
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://home.bytesbyboon.be/bookkeeping' // e.g. 'https://api.example.com' if backend hosted separately
+  apiBaseUrl: 'http://home.bytesbyboon.be:21595/bookkeeping' // e.g. 'https://api.example.com' if backend hosted separately
 };
